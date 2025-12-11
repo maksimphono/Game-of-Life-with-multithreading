c415cf926ca9b492b925a56e1ea525e2405857bd
maksimphono <trukhinmaksimof@mail.ru>
2025-12-12 00:50:25 +0800

    feat: defined a way of passing arguments to the worker

--- life-parallel.c
+++ life-parallel.c
@@ -28,13 +28,37 @@ void calculate_slice(LifeBoard* next_state, LifeBoard* old_state, int range[2]) 
     }
 }
 
-void* worker_function(LifeBoard* next_state, LifeBoard *state, int range[2]) {
+typedef struct {
+    LifeBoard* next_state;
+    LifeBoard* old_state;
+    int range[2];
+} worker_arguments_t;
+
+void* worker_function(void* args) {
     // this function is a start for each thread, each thread will run infinite loop here
+    worker_arguments_t* arguments = (worker_arguments_t*)args;
+    LifeBoard* next_state = arguments->next_state;
+    LifeBoard* state = arguments->old_state;
+    int range[2] = {arguments->range[0], arguments->range[1]};
 
 forever: // this loop will run forever, barrier will control it, when basically on each step of the game, barrier will wait for all threads to come and wait for all of them to finish their respective slice of the job
     
 
     goto forever;
+}
+
+void init_workers(LifeBoard* next_state, LifeBoard *state, pthread_t workers[], int workers_num) {
+    int board_size = state->width * state->height;
+    int range_length = board_size / workers_num;
+    int range[2] = {};
+    //void* args = malloc(sizeof(worker_arguments_t));
+    for (int i = 0; i < workers_num - 1; i++) {
+        // TODO: calculate start of the range, respecting the borders (borders must be 0 always)
+        range[START] = i * range_length;
+        range[END] = range[START] + range_length;
+        worker_arguments_t args = {next_state, state, range};
+        pthread_create(workers[i], NULL, worker_function, (void*)(&args));
+    }
 }
 
 void simulate_life_parallel(int threads, LifeBoard *state, int steps) {
@@ -49,5 +73,6 @@ void simulate_life_parallel(int threads, LifeBoard *state, int steps) {
 
     for (int step = 0; step < steps; step++) {
 
+        swap(next_state, state);
     }
 }
