d66a18e94e4baae014f7d3734f177cbe15087838
maksimphono <trukhinmaksimof@mail.ru>
2026-01-13 21:43:06 +0800

    feat: implemented cycled barrier methods

--- life-parallel.c
+++ life-parallel.c
@@ -29,3 +29,30 @@ int cyclic_barrier_init(cyclic_barrier_t* barrier, pthread_mutexattr_t* barrier_
 
     return 0;
 }
+
+
+
+int cyclic_barrier_await(cyclic_barrier_t* barrier) {
+    pthread_mutex_lock(&(barrier->barrier_mutex));
+
+    barrier->outstanding -= 1;
+
+    if (barrier->outstanding) {
+        pthread_cond_wait(&(barrier->barrier_cond), &(barrier->barrier_mutex));
+    } else {
+        (barrier->barrier_function)();
+        barrier->outstanding = barrier->participants;
+        pthread_cond_broadcast(&(barrier->barrier_cond));
+    }
+
+    pthread_mutex_unlock(&(barrier->barrier_mutex));
+
+    return 0;
+}
+
+int cyclic_barrier_destroy(cyclic_barrier_t* barrier) {
+    pthread_cond_destroy(&(barrier->barrier_cond));
+    pthread_mutex_destroy(&(barrier->barrier_mutex));
+
+    return 0;
+}
